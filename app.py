83f2d7f5ccaa5a4c45dd44f6f7dfe15c135a99b2
Ginga Kennis <gingakennis123@icloud.com>
2022-11-27 22:57:16 +0900

    commit

--- app.py
+++ app.py
@@ -7,12 +7,6 @@ net = Network([784,100,50,10])
 training_data,test_data = mnist_data_loader.create_data()
 net.SGD(training_data,100,10,0.25,test_data=test_data)
 
-""""""""""
-print('X_train: ' + str(train_X.shape))
-print('Y_train: ' + str(train_y.shape))
-print('X_test:  ' + str(test_X.shape))
-print('Y_test:  ' + str(test_y.shape))
-"""""""""
 
 
 
--- mnist_data_loader.py
+++ mnist_data_loader.py
@@ -3,13 +3,6 @@ import numpy as np
 
 
 
-"""""""""
-print('X_train: ' + str(train_X.shape))
-print('Y_train: ' + str(train_y.shape))
-print('X_test:  ' + str(test_X.shape))
-print('Y_test:  ' + str(test_y.shape))
-"""""""""
-
 def create_data():
     (train_X, train_y), (test_X, test_y) = mnist.load_data()
     all_training_data = []
